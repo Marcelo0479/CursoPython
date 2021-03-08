59b01b2dcaa5b8d8a0248601547524b4f3e04bdb
Marcelo0479 <melo137@gmail.com>
2021-03-08 17:37:00 -0300

    Aperfeiçoamento do codigo

--- Adiv2.py
+++ Adiv2.py
@@ -1,66 +1,79 @@
 import random
 
 def jogar():
-    print("Jogo de adivinhação 2")
 
-    numero_secreto = random.randrange(1,100)
+    print('Bem vindo ao jogo de advinhação')
+
+    numero_secreto = random.randrange(1,101)
+
+# Retirei a criação da variável de tentativas antes da atribuição do nível.
+# A própria criação do nível já cria a variável, portanto não a necessidade de criá-la antes.
 
     print("Defina a dificuldadde")
-    dificuldade = int(input("Digite 1 para fácil, 2 para médio, ou 3 para difícil: "))
-    if (dificuldade == 1):
+    nivel = int(input("Digite 1 para fácil, 2 para médio, ou 3 para difícil: "))
+
+    if (nivel == 1):
         tentativas = 20
-    elif (dificuldade == 2):
+    elif (nivel == 2):
         tentativas = 10
     else:
         tentativas = 5
 
-    total_de_tentativas = tentativas
     rodada = 1
     pontos = 1000
 
-    while (tentativas > 0):
-        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    for rodada in range(1, tentativas + 1 ):
+        print('Tentativa', rodada, 'de', tentativas)
 
-        rodada += 1
-        tentativas += 1
+#Usei a função try para que o código não quebre caso seja digitado algum valor inválido.
 
         try:
-            chute = int(input("Digite seu chute de 1 a 100: "))
+            chute = int(input("Digite seu chute: "))
 
             if (chute < 1 or chute > 100):
-                pontos = pontos - 100 * dificuldade
-                print("Você deve digitar um número entre 1 e 100. Você ainda temn {} pontos".format(pontos))
+                print('Vc deve digitar um número inteiro entre 1 e 100')
                 continue
 
-            acertou = chute == numero_secreto
+            acertou = numero_secreto == chute
             maior = chute > numero_secreto
+            menor = chute < numero_secreto
 
             if (acertou):
-                print("Acertou. Você fez {} pontos".format(pontos))
-                tentativas = 0
+                print('Parabens, vc acertou')
+                print('Vc fez {} pontos'.format(pontos))
                 break
-            elif (tentativas != 0):
+
+# Criei um elif aqui para que eu pudesse passar a mensagem que o jogardor perdeu.
+
+            elif (rodada < tentativas):
                 if (maior):
-                    pontos = pontos - abs(numero_secreto - chute) * dificuldade
-                    print(
-                        "Errou! Seu chute foi maior que o número secreto. Você ainda tem {} pontos. Tente novamente".format(
-                            pontos))
-                else:
-                    pontos = pontos - abs(numero_secreto - chute) * dificuldade
-                    print(
-                        "Errou! Seu chute foi menor que o número secreto. Você ainda tem {} pontos. Tente novamente".format(
-                            pontos))
+
+# Não criei a variável pontos perditos, pois ela não é necessária.
+# Criei uma print da pontuação atual apos cada chute.
+# Além disso, mpdifiquei a formula de calculo de perda de pontos, pois com a informação da pontuação parcial
+# o jogador poderia fazer a conta e descobrir o número secreto
+
+                    pontos = pontos - (10 * nivel)
+                    print('Vc errou. Seu chute foi maior que o número secreto.')
+                    print('Vc ainda tem {} pontos. Tente novamente'.format(pontos))
+                elif (menor):
+                    pontos = pontos - (10 * nivel)
+                    print('Vc errou. Seu chute foi menor que o número secreto.')
+                    print('Vc ainda tem {} pontos. Tente novamente'.format(pontos))
 
             else:
-                pontos = pontos - abs(numero_secreto - chute) * dificuldade
-                print("Perdeu. Você fez {} pontos.".format(pontos))
+                pontos = pontos - (10 * nivel)
+                print('Vc perdeu')
+                print('Vc fez {} pontos'.format(pontos))
 
         except ValueError:
-            pontos = pontos - 150 * dificuldade
-            print("Chute digitad não é valido. Você ainda tem {} pontos".format(pontos))
-            print("Digite um número entre 1 e 100.")
+            pontos = pontos - (10 * nivel)
+            print("O chute digitado não é válido. Digite um número inteiro entre 1 e 100.")
+            print('Vc ainda tem {} pontos. Tente novamente'.format(pontos))
 
-    print("Fim do jogo")
+        rodada += 1
+
+    print('Fim do jogo')
 
 if (__name__ == "__main__"):
     jogar()
